085dd05473fdf78d90d97a9db00059b83cc1235b
Dylan Anthony <dbanty@users.noreply.github.com>
2023-06-28 14:44:45 -0600

    Forat/delint Python coprocessor

--- coprocessors/python/main.py
+++ coprocessors/python/main.py
@@ -5,68 +5,84 @@ from uuid import uuid4 as uuid
 
 app = Flask(__name__)
 
-JWT_SECRET = environ.get('JWT_SECRET') if environ.get('JWT_SECRET') is not None else 'apollo'
+JWT_SECRET = (
+    environ.get("JWT_SECRET") if environ.get("JWT_SECRET") is not None else "apollo"
+)
+
 
 def _get_payload():
     payload = request.get_json(force=True)
-    if (payload.get('headers') is None):
-        payload['headers'] = {}
+    if payload.get("headers") is None:
+        payload["headers"] = {}
 
     return payload
+
 
 def _send_unauthenticated(payload):
-    payload['control'] = { 'break': 401 }
+    payload["control"] = {"break": 401}
     return payload
 
-@app.post('/client-awareness')
+
+@app.post("/client-awareness")
 def handle_client_awareness():
     payload = _get_payload()
-    if (payload['stage'] != 'RouterRequest'):
+    if payload["stage"] != "RouterRequest":
         return payload
 
-    if (payload['headers'].get('authentication') == None):
+    if payload["headers"].get("authentication") is None:
         return _send_unauthenticated(payload)
 
-    token = payload['headers']['authentication'][0].split('Bearer ')[1]
-    if (token == None):
+    token = payload["headers"]["authentication"][0].split("Bearer ")[1]
+    if token is None:
         return _send_unauthenticated(payload)
 
     try:
-        jwtPayload = jwt.decode(token, key=JWT_SECRET, algorithms='HS256')
-        clientName = [jwtPayload.get('client_name')] if jwtPayload.get('client_name') is not None else ['coprocessor']
-        clientVersion = [jwtPayload.get('client_version')] if jwtPayload.get('client_version') is not None else ['loadtest']
-        payload['headers']['apollographql-client-name'] = clientName
-        payload['headers']['apollographql-client-version'] = clientVersion
+        jwt_payload = jwt.decode(token, key=JWT_SECRET, algorithms="HS256")
+        client_name = (
+            [jwt_payload.get("client_name")]
+            if jwt_payload.get("client_name") is not None
+            else ["coprocessor"]
+        )
+        client_version = (
+            [jwt_payload.get("client_version")]
+            if jwt_payload.get("client_version") is not None
+            else ["loadtest"]
+        )
+        payload["headers"]["apollographql-client-name"] = client_name
+        payload["headers"]["apollographql-client-version"] = client_version
     except jwt.exceptions.InvalidSignatureError:
         return _send_unauthenticated(payload)
 
     return payload
 
-@app.post('/guid-response')
+
+@app.post("/guid-response")
 def handle_guid_response():
     payload = _get_payload()
-    if (payload['stage'] != 'RouterResponse'):
+    if payload["stage"] != "RouterResponse":
         return payload
 
-    payload['headers']['GUID'] = []
+    payload["headers"]["GUID"] = []
     for _ in range(10):
-        payload['headers']['GUID'].append(uuid())
+        payload["headers"]["GUID"].append(uuid())
 
     return payload
 
-@app.post('/static-subgraph')
+
+@app.post("/static-subgraph")
 def handle_static_subgraph():
     payload = _get_payload()
-    if (payload['stage'] != 'SubgraphRequest'):
+    if payload["stage"] != "SubgraphRequest":
         return payload
 
-    payload['headers']['source'] = ['coprocessor']
+    payload["headers"]["source"] = ["coprocessor"]
 
     return payload
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     from waitress import serve
 
-    port = int(environ.get('PORT')) if environ.get('PORT') is not None else 3000
-    print('Starting server on port', port)
+    port = int(environ.get("PORT")) if environ.get("PORT") is not None else 3000
+    print("Starting server on port", port)
     serve(app, port=port)
