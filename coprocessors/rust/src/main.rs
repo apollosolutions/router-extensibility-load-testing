947aa6918e15adb217d7f8e09aa8c294fe701f8a
Dylan Anthony <dbanty@users.noreply.github.com>
2023-06-28 14:44:34 -0600

    Add a Rust coprocessor

---
+++ coprocessors/rust/src/main.rs
@@ -0,0 +1,42 @@
+use axum::{response::IntoResponse, routing::post, Json, Router, Server};
+use serde_json::{Map, Value};
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new()
+        .route("/static-subgraph", post(static_subgraph))
+        .route("/guid-response", post(guid_response))
+        .route("/client-awareness", post(client_awareness));
+
+    Server::bind(&"0.0.0.0:8000".parse().unwrap())
+        .serve(router.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn static_subgraph(Json(mut body): Json<Map<String, Value>>) -> Json<Value> {
+    if body.get("stage").is_some_and(|value| {
+        value
+            .as_str()
+            .is_some_and(|stage| stage == "SubgraphRequest")
+    }) {
+        body.entry("headers")
+            .or_insert_with(|| Value::Object(Map::new()))
+            .as_object_mut()
+            .map(|headers| {
+                headers.insert(
+                    "source".to_string(),
+                    Value::Array(vec!["coprocessor".into()]),
+                )
+            });
+    }
+    Json(body.into())
+}
+
+async fn guid_response() -> impl IntoResponse {
+    todo!()
+}
+
+async fn client_awareness() -> impl IntoResponse {
+    todo!()
+}
