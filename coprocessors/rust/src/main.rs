256521061e41d174e427abffc79ab6e51556a6d2
Dylan Anthony <dbanty@users.noreply.github.com>
2023-06-30 12:51:25 -0600

    Add Rust guid

--- coprocessors/rust/src/main.rs
+++ coprocessors/rust/src/main.rs
@@ -33,8 +33,23 @@ async fn static_subgraph(Json(mut body): Json<Map<String, Value>>) -> Json<Value
     Json(body.into())
 }
 
-async fn guid_response() -> impl IntoResponse {
-    todo!()
+async fn guid_response(Json(mut body): Json<Map<String, Value>>) -> Json<Value> {
+    if body
+        .get("stage")
+        .and_then(|value| value.as_str())
+        .is_some_and(|stage| stage == "RouterResponse")
+    {
+        body.entry("headers")
+            .or_insert_with(|| Value::Object(Map::new()))
+            .as_object_mut()
+            .map(|headers| {
+                let guids = (0..10)
+                    .map(|_| uuid::Uuid::new_v4().to_string())
+                    .collect::<Vec<_>>();
+                headers.insert("GUID".to_string(), guids.into())
+            });
+    }
+    Json(body.into())
 }
 
 async fn client_awareness() -> impl IntoResponse {
